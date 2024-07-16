46a1e1345471fc463185e846748727b21b880b37
sergey-komar <kursev81@yandex.ru>
2024-07-16 19:55:39 +0300

    work

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,46 @@ $(function () {
         slidesToScroll: 1
       
      });
-})
+     $('.slider-services--one').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1
+        });
+     
+    $('.slider-services--two').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+      
+     });
+    $('.slider-services--three').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+      
+     });
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const menu = document.querySelector(".menu");
+    const mobile = document.querySelector(".nav-icon");
+  
+    mobile.addEventListener("click", function () {
+     document.body.classList.toggle('mobile-class');
+      this.classList.toggle("nav-icon--active");
+      menu.classList.toggle("nav--active");
+      document.body.classList.toggle('overflow');
+    });
+
+
+    // Маска для Инпута
+var element = document.getElementById('input-mask');
+var maskOptions = {
+	mask: '+{7}(000)000-00-00'
+};
+if(element){
+    var mask = IMask(element, maskOptions);
+}
+
+
+
+});
