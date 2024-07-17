074c0114623d60c424268086073b9ce65482d5c8
sergey-komar <kursev81@yandex.ru>
2024-07-17 18:18:23 +0300

    3page

--- app/js/main.js
+++ app/js/main.js
@@ -75,6 +75,19 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    const logoModile = document.querySelector(".logo-mobile");
+
+    if (logoModile) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY) {
+          logoModile.classList.add("logo-mobile-hidden");
+        } else {
+          logoModile.classList.remove("logo-mobile-hidden");
+        }
+      });
+    }
+
+
     // Маска для Инпута
 var element = document.getElementById('input-mask');
 var maskOptions = {
@@ -83,6 +96,74 @@ var maskOptions = {
 if(element){
     var mask = IMask(element, maskOptions);
 }
+
+
+const headingBlock = document.querySelector(".heading-block");
+  const headingVideo = document.querySelector(".heading__video--about");
+  var videoEl = document.getElementsByTagName("video")[0],
+    playBtn = document.getElementById("playBtn"),
+    vidControls = document.getElementById("controls");
+
+  if (playBtn) {
+    playBtn.addEventListener("click", () => {
+      headingBlock.classList.toggle("block-hidden");
+    });
+  }
+
+  if (videoEl && vidControls && playBtn && headingVideo) {
+    // если браузер может воспроизводить видео удаляем класс
+    videoEl.addEventListener(
+      "canplaythrough",
+      function () {
+        vidControls.classList.remove("hidden");
+      },
+      false
+    );
+    // запускам или останавливаем воспроизведение
+    playBtn.addEventListener(
+      "click",
+      function () {
+        if (videoEl.paused) {
+          videoEl.play();
+        } else {
+          videoEl.pause();
+        }
+        headingVideo.classList.toggle("hidden-video");
+        const path = videoEl.getAttribute("controls");
+        videoEl.setAttribute("controls", path);
+      },
+      false
+    );
+  }
+
+  if (videoEl && playBtn) {
+    videoEl.addEventListener(
+      "play",
+      function () {
+        playBtn.innerText = "";
+      },
+      false
+    );
+
+    videoEl.addEventListener(
+      "pause",
+      function () {
+        playBtn.innerText = "";
+      },
+      false
+    );
+  }
+
+  if (videoEl) {
+    videoEl.addEventListener(
+      "ended",
+      function () {
+        videoEl.currentTime = 0;
+      },
+      false
+    );
+  }
+
 
 
 
